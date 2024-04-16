274aa97b2b4504bc0d443454b86a12497db024a0
Daniel Caldera <bdcaldera@gmail.com>
2024-04-15 21:59:48 -0600

    feat: min fixes

--- backups/postgres/restore.ts
+++ backups/postgres/restore.ts
@@ -3,25 +3,29 @@
 // By Daniel Caldera
 // restore.ts
 
-import fs from "fs";
-import { $ } from "bun";
+import fs from 'fs'
+import { $ } from 'bun'
 
-const database_url = process.env.DATABASE_URL;
+const database_url = process.env.DATABASE_URL
 
-const args = process.argv.slice(2);
+const args = process.argv.slice(2)
 
 async function restore() {
-  const compressedBackupFile = args[0];
+  const compressedBackupFile = args[0]
   if (!compressedBackupFile) {
-    console.log("No backup file provided");
-    return 1;
+    console.log('No backup file provided')
+    return 1
   }
-  const data = fs.readFileSync(compressedBackupFile);
-  const decompressed = Bun.gunzipSync(data);
+  const data = fs.readFileSync(compressedBackupFile)
 
-  await $`psql '${database_url}' < ${decompressed}`;
+  // comment the line below to restore if file is not compressed
+  const decompressed = Bun.gunzipSync(data)
 
-  console.log("Restored");
+  // uncoment the line below to restore if file is not compressed
+  // await $`psql '${database_url}' < ${data}`
+  await $`psql '${database_url}' < ${decompressed}`
+
+  console.log('Restored')
 }
 
-restore();
+restore()
