8e3688b47e6d9f02dc5e900661b159ccaa845d0b
Daniel Caldera Rosas <dancaldera@proton.me>
2024-05-06 13:04:43 -0600

    chore: Refactor backup script for PostgreSQL

--- backups/postgres/backup.ts
+++ backups/postgres/backup.ts
@@ -3,22 +3,22 @@
 // By Daniel Caldera
 // manual-backup.ts
 
-import fs from "fs";
-import { $ } from "bun";
+import fs from 'fs'
+import { $ } from 'bun'
 
-const db = process.env.DATABASE_URL;
+const db = process.env.DATABASE_URL
 
-const date = new Date();
-const currentDate = `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}.${date.getHours()}.${date.getMinutes()}`;
-const backupFile = `pg-backup-${currentDate}`;
+const date = new Date()
+const currentDate = `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}.${date.getHours()}.${date.getMinutes()}`
+const backupFile = `pg-backup-${currentDate}`
 
 const backup = async () => {
-  await $`pg_dump '${db}' -f ${backupFile}`;
-  await $`tar -czvf ${backupFile}.tar.gz ${backupFile}`;
+  await $`pg_dump '${db}' -f ${backupFile}`
+  await $`tar -czvf ${backupFile}.tar.gz ${backupFile}`
 
-  fs.unlinkSync(backupFile);
+  fs.unlinkSync(backupFile)
 
-  console.log(`Backup created successfully`);
-};
+  console.log(`Backup created successfully`)
+}
 
-backup();
+backup()
